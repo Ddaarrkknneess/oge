fea468fb7a08ca2ab767dd6d6daba113e1c54f58
Ddaarrkknneess <smile0cake@gmail.com>
2019-09-26 22:05:15 +0300

    Create 361.pas

---
+++ task20/361.pas
@@ -0,0 +1,16 @@
+﻿var
+  amount, speed, maxSpeed, countAbove30: integer;
+  velocity: real;
+
+begin
+  read(amount);
+  for var i := 1 to amount do
+  begin
+    read(velocity);
+    speed := round(velocity);
+    if speed > maxSpeed then maxSpeed := speed;
+    if speed < 30 then countAbove30 += 1;
+  end;
+  writeln(maxSpeed);
+  if countAbove30 > 0 then write('YES') else write('NO');
+end.
