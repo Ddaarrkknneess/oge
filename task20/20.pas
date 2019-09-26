cfb5ccc71c1cdebe5596cbd3705f2acbd1ebfc5f
Ddaarrkknneess <smile0cake@gmail.com>
2019-09-26 21:39:53 +0300

    20 done

---
+++ task20/20.pas
@@ -0,0 +1,13 @@
+﻿var
+  amount, max, number: integer;
+  
+begin
+  read(amount);
+  max := 0;
+  for var i := 1 to amount do
+  begin
+    read(number);
+    if (number mod 5 = 0) and (number > max) then max := number;
+  end;
+  write(max);
+end.
