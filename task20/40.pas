e57bc58bea13791739ed2bfe6b7e5f8f6eaf5363
Ddaarrkknneess <smile0cake@gmail.com>
2019-09-26 21:41:37 +0300

    Create 40.pas

---
+++ task20/40.pas
@@ -0,0 +1,13 @@
+﻿var
+  amount, sum, number: integer;
+  
+begin
+  read(amount);
+  sum := 0;
+  for var i := 1 to amount do
+  begin
+    read(number);
+    if number mod 6 = 0 then sum += number;
+  end;
+  write(sum);
+end.
