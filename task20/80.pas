5f3d97a3d8c28631a3c2a3dad6504208dfcb9df2
Ddaarrkknneess <smile0cake@gmail.com>
2019-10-17 22:22:43 +0300

    80 done

---
+++ task20/80.pas
@@ -0,0 +1,13 @@
+﻿var
+  amount, min, number: integer;
+  
+begin
+  read(amount);
+  min := integer.MaxValue;
+  for var i := 1 to amount do
+  begin
+    read(number);
+    if (number mod 3 = 0) and (number < min) then min := number;
+  end;
+  write(min);
+end.
