fae430dbdf9c4b82d046e15f59a260af4b384339
Qvocha <pineappletoe@gmail.com>
2019-11-21 20:29:27 +0300

    Create 1.pas

---
+++ 21112019/1.pas
@@ -0,0 +1,40 @@
+﻿const
+  n = 10;
+
+var
+  a: array[1..n] of integer;
+  min, max, minIndex, maxIndex: integer;
+
+begin
+  for var i := 1 to n do
+  begin
+    a[i] := random(1, 100);
+    write(a[i], ' ');
+  end;
+  min := integer.MaxValue;
+  max := integer.MinValue;
+  for var i := 1 to n do
+  begin
+    if a[i] < min then min := a[i];
+    if a[i] > max then max := a[i];
+  end;
+  for var i := 1 to n do
+    if a[i] = min then
+    begin
+      minIndex := i;
+      break;
+    end;
+  for var i := 1 to n do
+    if a[i] = max then
+    begin
+      maxIndex := i;
+      break;
+    end;
+  a[minIndex] := max;
+  a[maxIndex] := min;
+  writeln;
+  for var i := 1 to n do
+  begin
+    write(a[i], ' ');
+  end;
+end.
